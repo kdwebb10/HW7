1b3e1307a01cac802ffdfe19e679babaedff33a3
kyle webb <kylewebb4@weber.edu>
2017-03-22 22:19:47 +0000

    Wrote part of function

--- butler_hw7_task2.c
+++ butler_hw7_task2.c
@@ -13,8 +13,9 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>		/* Standard Library */
-#include <math.h>
 #include <time.h>		/* to gain cpu time */
+
+#define MAXNUM 10 //define a max number for list of intagers
 
 /* Function Prototypes */
 void MaxMin(int numvals, int vals[], int* min, int* max);
@@ -22,11 +23,22 @@ void MaxMin(int numvals, int vals[], int* min, int* max);
 /* Main Program */
 int main(int argc, char *argv[])
 {
-	int max, min, random[];
-	MaxMin();
-
+	int max, min, random[MAXNUM];
+	MaxMin(MAXNUM, &min, &max, random);
 	return 0;
 }
 /* Function Defenitions */
+void MaxMin(int numvals, int vals[], int* min, int* max)
+{
+	srand(time(NULL)); //sets your random seed
+
+	printf("The list of numbers is:\n");
+
+	printf("The Maximum value in the series is %d\n", *max);
+	printf("The Minimum value in the series is %d\n", *min);
+
+	return;
+
+}
 
 
